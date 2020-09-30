fecc8f2f134b6b2aa9147b346eec39d7cc3f3d90
lenmon54231 <limeng54231@gmail.com>
2020-09-30 16:45:12 +0800

    addcutcircle

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -4,27 +4,38 @@
       <el-row>
         <el-col :xs="24" :sm="4" :md="18" :lg="18" :xl="18">
           <div class="main">
-            <list_home :items="items"></list_home>
+            <ListHome :items="items"></ListHome>
           </div>
         </el-col>
-        <el-col :xs="0" :sm="4" :md="6" :lg="6" :xl="6"></el-col>
+        <el-col :xs="0" :sm="4" :md="6" :lg="6" :xl="6">
+          <div id="pic" @click="cut">
+            <el-image
+              ref="originImg"
+              src="../../static/img/avatar.png"
+            ></el-image>
+          </div>
+          <div v-if="circleImg">
+            <el-image :src="circleImg"></el-image>
+          </div>
+        </el-col>
       </el-row>
     </div>
   </div>
 </template>
 
 <script>
-import List_home from "../components/list_home";
+import ListHome from "../components/list_home";
 import { webUrl } from "../../static/js/public.js";
 export default {
   data() {
     return {
       items: [],
       isLoading: false,
+      circleImg: null,
     };
   },
   components: {
-    List_home,
+    ListHome,
   },
   created() {
     this.isLoading = true;
@@ -32,6 +43,79 @@ export default {
       this.isLoading = false;
       this.items = res.data.reverse();
     });
+  },
+  methods: {
+    cut() {
+      let img = this.$refs.originImg;
+      this.circleImg = this.circle_image_v2(img);
+    },
+    circle_image_v2(img, imgType) {
+      let imgNode = img.$el.children[0];
+      let type = imgType || 0;
+      let radius, diameter, ctx, natural;
+      console.log(img, "img.naturalWidth");
+      if (type) {
+        if (img.naturalWidth > img.naturalHeight) {
+          radius = img.naturalHeight / 2;
+        } else {
+          radius = img.naturalWidth / 2;
+        }
+      } else {
+        if (img.imageWidth > img.imageHeight) {
+          radius = img.imageHeight / 2;
+        } else {
+          radius = img.imageWidth / 2;
+        }
+        if (img.imageWidth > img.imageHeight) {
+          natural = img.imageHeight;
+        } else {
+          natural = img.imageWidth;
+        }
+      }
+      diameter = radius * 2;
+      let canvas = document.createElement("canvas");
+      let contex = canvas.getContext("2d");
+      if (!canvas.getContext) {
+        // 判断浏览器是否支持canvas，如果不支持在此处做相应的提示
+        console.log("您的浏览器版本过低，暂不支持。");
+        return false;
+      }
+      canvas.width = diameter;
+      canvas.height = diameter;
+      contex.clearRect(0, 0, diameter, diameter);
+      contex.save();
+      contex.beginPath();
+      contex.arc(radius, radius, radius, 0, Math.PI * 2, false);
+      contex.clip();
+      if (type) {
+        contex.drawImage(
+          imgNode,
+          0,
+          0,
+          diameter,
+          diameter,
+          0,
+          0,
+          diameter,
+          diameter
+        );
+      } else {
+        contex.drawImage(
+          imgNode,
+          0,
+          0,
+          natural,
+          natural,
+          0,
+          0,
+          diameter,
+          diameter
+        );
+      }
+      contex.restore();
+      console.log(natural, diameter);
+      return canvas.toDataURL("image/png");
+    },
   },
 };
 </script>
@@ -91,6 +175,13 @@ export default {
       }
     }
   }
+}
+#pic:hover {
+  transform: rotate(666turn);
+  transition-property: all;
+  transition-duration: 59s;
+  transition-timing-function: cubic-bezier(0.34, 0, 0.84, 1);
+  transition-delay: 1s;
 }
 
 @media (min-width: 768px) {
